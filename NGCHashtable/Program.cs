1fd84a98d60d13bb8c096795ff04385a7656e602
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-22 16:43:37 +0530

    Add project files.

---
+++ NGCHashtable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGCHashtable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add(1, "2000");
+            ht.Add(2, "678");
+            ht.Add(3, 4555);
+            ht.Remove(2);
+            foreach (DictionaryEntry d in ht)
+            {
+                Console.WriteLine( $"{d.Key}  {d.Value} ");
+            }
+        }
+    }
+}
